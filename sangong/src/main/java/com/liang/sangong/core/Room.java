2578e1a5c86818707b6d292a73058e56e52892e4
梁志彦 <kiven@192.168.0.103>
2019-03-06 23:44:41 +0800

    xxx

--- sangong/src/main/java/com/liang/sangong/core/Room.java
+++ sangong/src/main/java/com/liang/sangong/core/Room.java
@@ -3,110 +3,127 @@ package com.liang.sangong.core;
 import com.liang.sangong.bo.OnePoke;
 import com.liang.sangong.core.PeoplePlay.GameType;
 import com.liang.sangong.core.PeoplePlay.PeopleState;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
 public class Room {
 
-  private int minPeopleNumber = 2;
-  private int maxPeopleNumber = 6;
+    private int minPeopleNumber = 2;
+    private int maxPeopleNumber = 6;
 
-  private List<PeoplePlay> peoplePlayList = new ArrayList<>();
+    private List<PeoplePlay> peoplePlayList = new ArrayList<>();
 
-  private GameType gameType;
+    private GameType gameType;
 
-  private volatile boolean begin;
+    private volatile boolean begin;
 
-  private String roomId;
+    private String roomId;
 
-  public enum TableState {
-    INIT, ALL_CONFIRM, DOING;
-  }
-
-  public Room(GameType gameType) {
-    this.gameType = gameType;
-    this.roomId = UUID.randomUUID().toString().replaceAll("-", "");
-  }
-
-  public TableState getState() {
-    boolean confirm = false;
-    if (peoplePlayList.size() < minPeopleNumber) {
-      return TableState.INIT;
+    public enum TableState {
+        INIT, ALL_CONFIRM, DOING;
     }
-    for (PeoplePlay peoplePlay : peoplePlayList) {
-      if (peoplePlay.getState() == PeopleState.INIT) {
-        return TableState.INIT;
-      } else if (peoplePlay.getState() == PeopleState.CONFIRM) {
-        confirm = true;
-      }
+
+    public Room(GameType gameType) {
+        this.gameType = gameType;
+        this.roomId = UUID.randomUUID().toString().replaceAll("-", "");
     }
-    return confirm == true ? TableState.ALL_CONFIRM : TableState.DOING;
-  }
 
-  public synchronized boolean add(PeoplePlay peoplePlay) {
-    if (isCanAdd() && getState() == TableState.INIT && !begin) {
-      peoplePlay.setRoom(this);
-      peoplePlay.setGameType(gameType);
-      return peoplePlayList.add(peoplePlay);
+    public TableState getState() {
+        boolean confirm = false;
+        if (peoplePlayList.size() < minPeopleNumber) {
+            return TableState.INIT;
+        }
+        for (PeoplePlay peoplePlay : peoplePlayList) {
+            if (peoplePlay.getState() == PeopleState.INIT) {
+                return TableState.INIT;
+            } else if (peoplePlay.getState() == PeopleState.CONFIRM) {
+                confirm = true;
+            }
+        }
+        return confirm == true ? TableState.ALL_CONFIRM : TableState.DOING;
     }
-    return false;
-  }
 
-  public void fapai() {
-    OnePoke onePoke = new OnePoke();
-    for (int i = 0; i < 3; i++) {
-      for (PeoplePlay peoplePlay : peoplePlayList) {
-        peoplePlay.addPoke(onePoke.pop());
-      }
+    public synchronized boolean add(PeoplePlay peoplePlay) {
+        if (isCanAdd() && peoplePlay.getState() == PeopleState.INIT && getState() == TableState.INIT
+                && !begin) {
+            peoplePlay.setRoom(this);
+            peoplePlay.setGameType(gameType);
+            return peoplePlayList.add(peoplePlay);
+        }
+        return false;
     }
-  }
 
-  public void compare() {
-    PeoplePlay winner = winner();
-    if (gameType == GameType.ZIDONG) {
-
-    } else {
-
+    public synchronized boolean leave(PeoplePlay peoplePlay) {
+        if (peoplePlay.getState() == PeopleState.INIT && getState() == TableState.INIT && !begin) {
+            for (Iterator<PeoplePlay> iterator = peoplePlayList.iterator(); iterator.hasNext(); ) {
+                PeoplePlay peoplePlayNext = iterator.next();
+                if (peoplePlayNext.getPeopleInfo().getId() == peoplePlay.getPeopleInfo().getId()) {
+                    peoplePlayNext.setRoom(null);
+                    iterator.remove();
+                    break;
+                }
+            }
+        }
+        return true;
     }
-    System.out.println("winner: " + winner.getCurrentPoke());
-  }
 
-  private PeoplePlay winner() {
-    PeoplePlay winner = peoplePlayList.get(0);
-    for (int i = 1; i < peoplePlayList.size(); i++) {
-      winner = Rule.compareRetrunWinner(winner, peoplePlayList.get(i));
+    public void fapai() {
+        OnePoke onePoke = new OnePoke();
+        for (int i = 0; i < 3; i++) {
+            for (PeoplePlay peoplePlay : peoplePlayList) {
+                peoplePlay.addPoke(onePoke.pop());
+            }
+        }
     }
-    return winner;
-  }
 
-  public boolean isCanAdd() {
-    return peoplePlayList.size() < maxPeopleNumber;
-  }
+    public void compare() {
+        PeoplePlay winner = winner();
+        if (gameType == GameType.ZIDONG) {
 
-  public List<PeoplePlay> getPeoplePlayList() {
-    return peoplePlayList;
-  }
+        } else {
 
-  public boolean isBegin() {
-    return begin;
-  }
+        }
+        System.out.println("winner: " + winner.getCurrentPoke());
+    }
 
-  public Room setBegin(boolean begin) {
-    this.begin = begin;
-    return this;
-  }
+    private PeoplePlay winner() {
+        PeoplePlay winner = peoplePlayList.get(0);
+        for (int i = 1; i < peoplePlayList.size(); i++) {
+            winner = Rule.compareRetrunWinner(winner, peoplePlayList.get(i));
+        }
+        return winner;
+    }
 
-  public GameType getGameType() {
-    return gameType;
-  }
+    public boolean isCanAdd() {
+        return peoplePlayList.size() < maxPeopleNumber;
+    }
 
-  public Room setGameType(GameType gameType) {
-    this.gameType = gameType;
-    return this;
-  }
+    public List<PeoplePlay> getPeoplePlayList() {
+        return peoplePlayList;
+    }
 
-  public String getRoomId() {
-    return roomId;
-  }
+    public boolean isBegin() {
+        return begin;
+    }
+
+    public Room setBegin(boolean begin) {
+        this.begin = begin;
+        return this;
+    }
+
+    public GameType getGameType() {
+        return gameType;
+    }
+
+    public Room setGameType(GameType gameType) {
+        this.gameType = gameType;
+        return this;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
 }
