b8da0d989447677a56a3832b58a479b246266690
lvs007 <zy.liang.5@163.com>
2021-08-09 18:18:41 +0800

    mdy

--- day/src/main/resources/static/js/common.js
+++ day/src/main/resources/static/js/common.js
@@ -3,15 +3,15 @@ document.write("<script type='text/javascript' src='/static/js/bignumber.js'></s
 // backend.js
 const ipfs = "https://ipfs.io/ipfs/";
 /*main network*/
-//const local = "http://gateway.daynft.org/ipfs/"
-//const ipAddr = "ipfs.daynft.org";//"api.btfs.trongrid.io";
-//const ipPort = "80";//"443";
-//const gatePort = "80";
+const local = "http://gateway.daynft.org/ipfs/"
+const ipAddr = "ipfs.daynft.org";//"api.btfs.trongrid.io";
+const ipPort = "80";//"443";
+const gatePort = "80";
 /*local*/
-const local = "http://127.0.0.1:8080/ipfs/"
-const ipAddr = "127.0.0.1";//"api.btfs.trongrid.io";
-const ipPort = "5001";//"443";
-const gatePort = "8080";
+//const local = "http://127.0.0.1:8080/ipfs/"
+//const ipAddr = "127.0.0.1";//"api.btfs.trongrid.io";
+//const ipPort = "5001";//"443";
+//const gatePort = "8080";
 
 const imgContractAdd = "TPhyBmRfk5RKSYR3Dj2kK74UNTS3RMMzcd";
 const vedioContractAdd = "TJn4ohdXDm6HFE4mSfj9jQxj9QnafcP64y";
